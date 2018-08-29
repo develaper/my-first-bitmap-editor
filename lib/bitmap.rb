7d7cee55e8308f81a4cd67680ba78e90172d96f4
develaper <lachguer.brahim@gmail.com>
2018-08-29 11:48:24 +0200

    Adds validation for column and row value before accessing to a pixel

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -7,5 +7,18 @@ class Bitmap
 
 	def [](i, j)
 		@bitmap[i-1][j-1]
+		if row_in_range?(i) and column_in_range?(j)
+			@bitmap[i-1][j-1]
+		else
+			fail "Values out of range"
+		end
+	end
+
+	def row_in_range?(n)
+		n.to_i.between?(1, @rows_number)
+	end
+
+	def column_in_range?(m)
+		m.to_i.between?(1, @columns_number)
 	end
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,7 +1,7 @@
 require 'bitmap'
 
 describe Bitmap do
-	context 'acces to pixel' do
+	context 'access to a specific pixel' do
 		it 'returns the value for an specific pixel' do
 			bitmap = Bitmap.new(2, 2)
 			expect(bitmap[1,1]).to eq('O')
@@ -15,6 +15,30 @@ describe Bitmap do
 			expect(bitmap[1,2]).to eq('O')
 			expect(bitmap[2,1]).to eq('O')
 			expect(bitmap[2,2]).to eq('O')
+		end
+	end
+
+	context 'row in range' do
+		it 'returns false if the coordinate is not in range' do
+			bitmap = Bitmap.new(5, 5)
+			expect(bitmap.row_in_range?(6)).to be_falsey
+		end
+
+		it 'returns true if the coordinate is in range' do
+			bitmap = Bitmap.new(5, 5)
+			expect(bitmap.row_in_range?(4)).to be_truthy
+		end
+	end
+
+	context 'column in range' do
+		it 'returns false if the coordinate is not in range' do
+			bitmap = Bitmap.new(5, 5)
+			expect(bitmap.column_in_range?(6)).to be_falsey
+		end
+
+		it 'returns true if the coordinate is in range' do
+			bitmap = Bitmap.new(5, 5)
+			expect(bitmap.column_in_range?(5)).to be_truthy
 		end
 	end
 end
